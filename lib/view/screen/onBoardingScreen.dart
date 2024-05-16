70fec22fbf23abf078151504b098db85316a9bed
MostafaMahm0ud <engmostafamahm0oud@gmail.com>
2024-05-16 10:50:39 -0700

    start of on bording and end of file structure and images & fonts

---
+++ lib/view/screen/onBoardingScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: const Text('Hi'),
+        ),
+      ),
+    );
+  }
+}
